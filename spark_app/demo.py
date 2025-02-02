f9bb84faf9228d33951a8eb7a08a2a21ea92361b
cappymayor <mayowaakinyele40@gmail.com>
2025-02-02 16:51:29 +0100

    remove pyanalyze

--- spark_app/demo.py
+++ spark_app/demo.py
@@ -1,15 +1,19 @@
-from pyspark.sql import SparkSession
+# from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("demo1").getOrCreate()
+# spark = SparkSession.builder.appName("demo1").getOrCreate()
 
-df = spark.createDataFrame(
-    [
-        ("sue", 32),
-        ("li", 3),
-        ("bob", 75),
-        ("heo", 13),
-    ],
-    ["first_name", "age"],
-)
+# df = spark.createDataFrame(
+#     [
+#         ("sue", 32),
+#         ("li", 3),
+#         ("bob", 75),
+#         ("heo", 13),
+#     ],
+#     ["first_name", "age"],
+# )
 
-df.show()
+# df.show()
+# if __name__ == "__main__":
+#     print("hello")
+
+#     print(5/0)
