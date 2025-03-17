db8bba5f5de655698ca33d08374d0434996ee79b
AyoDennis <ayodejidennis33@gmail.com>
2025-03-17 20:08:14 +0100

    create remote statefile

--- infrastructure/vpc_stacks.tf
+++ infrastructure/vpc_stacks.tf
@@ -12,17 +12,30 @@ resource "aws_vpc" "emr-vpc" {
   }
 }
 
-# Creating public subnet
-resource "aws_subnet" "public-subnet" {
+# Creating public subnet 1
+resource "aws_subnet" "public-subnet-1" {
   vpc_id     = aws_vpc.emr-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+# Creating public subnet 2
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id     = aws_vpc.emr-vpc.id
+  cidr_block = "10.0.2.0/24"
   availability_zone = "eu-central-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-2"
   }
 }
+
 
 # Creating internet gateway and attaching to VPC
 resource "aws_internet_gateway" "igw" {
@@ -47,8 +60,14 @@ resource "aws_route_table" "public-rt" {
   }
 }
 
-# Making explicit association of public subnet to route table
-resource "aws_route_table_association" "public-subnet-association" {
-  subnet_id      = aws_subnet.public-subnet.id
+# Making explicit association of public subnet 1 to route table
+resource "aws_route_table_association" "public-subnet-association-1" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+# Making explicit association of public subnet 2 to route table
+resource "aws_route_table_association" "public-subnet-association-2" {
+  subnet_id      = aws_subnet.public-subnet-2.id
   route_table_id = aws_route_table.public-rt.id
 }
