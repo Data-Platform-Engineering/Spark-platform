875cc5908f9be4855938d80ea46477b21a35883c
AyoDennis <ayodejidennis33@gmail.com>
2025-03-17 19:26:50 +0100

    further modifications

--- infrastructure/vpc_stacks.tf
+++ infrastructure/vpc_stacks.tf
@@ -1,35 +1,21 @@
-
 # Provisioning the VPC where EMR will be deployed
-
 resource "aws_vpc" "emr-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
   tags = {
-    Name = "production-emr"
+    Name = "custom-vpc"
     environment = "production"
     owner = "data_platform_team"
     managed_by = "terraform"
   }
 }
 
-# Creating private subnet
-resource "aws_subnet" "private-subnet" {
-  vpc_id     = aws_vpc.emr-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-central-1a"
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
-
 # Creating public subnet
 resource "aws_subnet" "public-subnet" {
   vpc_id     = aws_vpc.emr-vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.1.0/24"
   availability_zone = "eu-central-1b"
   map_public_ip_on_launch = true
 
@@ -39,11 +25,11 @@ resource "aws_subnet" "public-subnet" {
 }
 
 # Creating internet gateway and attaching to VPC
-resource "aws_internet_gateway" "emr-igw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.emr-vpc.id
 
   tags = {
-    Name = "emr-igw"
+    Name = "custom-igw"
   }
 }
 
@@ -53,7 +39,7 @@ resource "aws_route_table" "public-rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.emr-igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
@@ -61,10 +47,8 @@ resource "aws_route_table" "public-rt" {
   }
 }
 
-
 # Making explicit association of public subnet to route table
 resource "aws_route_table_association" "public-subnet-association" {
   subnet_id      = aws_subnet.public-subnet.id
   route_table_id = aws_route_table.public-rt.id
 }
-
