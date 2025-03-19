c73e75df145350895272fd914a40e2b17e736c39
cappymayor <mayowaakinyele40@gmail.com>
2025-03-19 16:17:56 +0100

    emr spark job schedule

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,8 @@
-# # Terraform Remote State
-# terraform {
-#   backend "s3" {
-#     bucket = "custom-emr-state-bucket"
-#     key    = "key/terraform.tfstate"
-#     region = "eu-central-1"
-#   }
-# }
+# Terraform Remote State
+terraform {
+  backend "s3" {
+    bucket = "custom-emr-state-bucket"
+    key    = "key/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
