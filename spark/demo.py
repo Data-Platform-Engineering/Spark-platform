cf4c0869426816158edba1ac85dc30e76de1acec
cappymayor <mayowaakinyele40@gmail.com>
2025-03-20 08:57:00 +0100

    schedule

--- spark/demo.py
+++ spark/demo.py
@@ -3,5 +3,4 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("demo").getOrCreate()
 
 df = spark.read.parquet("s3a://spark-job-source-datasets/random_user_profile/")
-
 df.show()
